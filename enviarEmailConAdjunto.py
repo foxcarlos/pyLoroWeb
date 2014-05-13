dbb4fa693c5f316410e04c72df67539f98d3c576
FoxCarlos Trabajo <foxcarlos@gmail.com>
2014-05-13 08:44:12 -0430

    modified:   enviarEmailConAdjunto.py Se creo para el correo hotmail

--- enviarEmailConAdjunto.py
+++ enviarEmailConAdjunto.py
@@ -1,9 +1,11 @@
+# Import smtplib for the actual sending function
 import smtplib
+
+# Here are the email package modules we'll need
 from email.mime.text import MIMEText
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email.MIMEImage import MIMEImage
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import sys
 
 class correo:
     def __init__(self):
@@ -11,7 +13,7 @@ class correo:
 
     def leerArchivo(self):
         listaEmail = []
-        f = '/home/foxcarlos/desarrollo/python/pyLoroWeb/correos2'
+        f = '/home/administrador/desarrollo/python/pyLoroWeb/correos'
         fi = open(f)
         leido = fi.read()
         lista = leido.split(',')
@@ -20,7 +22,7 @@ class correo:
             listaEmail.append(fila.strip())
         return listaEmail
     
-    def enviar_email(self, destinatario, mensaje, imgAdjunta, remitente='pycondor@gmail.com', asunto='** Sistema de Monitoreo y Control Condor **'):
+    def enviar_email(self, destinatario, mensaje, imgAdjunta, remitente='congresoshospitalcoromoto@gmail.com', asunto='Congresos Hospital Coromoto'):
         '''
         El metodo enviar email recibe 3 parametros:
         Destinatario:uncorreo@gmail.com
@@ -28,24 +30,20 @@ class correo:
         Remitente:Si no se envia ningun remitente el toma por defecto pycondor@gma
         Asunto:si no se coloca ninguno el toma por defecto el pasado en el parametr
         MEMultipart()'''
+
+        #COMMASPACE = ', '
         
-        # Creamos el mensaje
-        #msg = MIMEText(mensaje)
-        msg = MIMEMultipart()  # 'alternative')  # MIMEText(mensaje)
-        
-        # Conexin con el server
+        # Create the container (outer) email message.
+        msg = MIMEMultipart()
         msg['Subject'] = asunto
+        # me == the sender's email address
+        # family = the list of all recipients' email addresses
         msg['From'] = remitente
-        msg['To'] = destinatario
-    
+        msg['To'] = destinatario  # COMMASPACE.join(destinatario)
+        msg.preamble = mensaje
+
         #Creamos el cuerpo del mensaje
         msg.attach(MIMEText(mensaje))
-
-        '''# Adjuntamos Imagen
-        file = open(imgAdjunta, "rb")
-        attach_image = MIMEImage(file.read())
-        attach_image.add_header('Content-Disposition', 'attachment; filename = "imgAdjunta"')
-        msg.attach(attach_image)'''
 
         archivo_imagen = open(imgAdjunta, 'rb')
         msgImage = MIMEImage(archivo_imagen.read())
@@ -59,21 +57,25 @@ class correo:
         msg.attach(msgImage)
 
         # Autenticamos
-        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer = smtplib.SMTP('smtp.live.com', 587)
         mailServer.ehlo()
         mailServer.starttls()
         mailServer.ehlo()
-        mailServer.login("pycondor@gmail.com", "shc21152115")
+        mailServer.login("congresoshospitalcoromoto@outlook.com", "docencia2014")
     
         # Enviamos
         print(destinatario)
-        mailServer.sendmail(remitente, destinatario, msg.as_string())
+        try:
+            mailServer.sendmail(remitente, destinatario, msg.as_string())
+        except:
+            print(destinatario, sys.exc_info()[1])
+
         # Cerramos conexion
         mailServer.close()
     
 if __name__ == '__main__':
     app = correo()
     l = app.leerArchivo()
-    ll = ','.join(l)
-    app.enviar_email('foxcarlos@hotmail.com foxcarlos@gmail.com', 'Prueba', '/home/foxcarlos/desarrollo/python/pyLoroWeb/congreso.jpg', 'pycondor@gmail.com', 'Enviando desde python')
-    
+    #ll = ','.join(l)
+    for destino in l:
+        app.enviar_email(destino, 'Invitacion al II Congreso y III Jornadas Cientificas Integrales del Hospital Coromoto.  Si no puede ver la imagen haga click aqui: http://foxcarlos.no-ip.biz:8085/congreso', '/home/administrador/desarrollo/python/pyLoroWeb/congreso.jpg', 'congresoshospitalcoromoto@outlook.com', 'Invitacvion al II Congreso y III Jornadas Cientificas del Hospital Coromoto')
