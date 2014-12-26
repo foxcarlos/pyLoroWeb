f9acf6bb935a64e0a54215fa1a703512ecbdb152
FoxCarlos Trabajo <foxcarlos@gmail.com>
2014-12-26 15:50:41 -0430

    pruebas con instagram

--- pyLoroInstagram.py
+++ pyLoroInstagram.py
@@ -14,4 +14,4 @@ access_token = "104740353.689a9b6.abc540ee313849eb84992578a08fee0f"
 api = InstagramAPI(access_token=access_token)
 recent_media, next_ = api.user_recent_media(user_id="689a9b6752e74f01afe8242ccbcd03bc", count=10)
 for media in recent_media:
-   print media.caption.text
+   print(media.caption.text)
